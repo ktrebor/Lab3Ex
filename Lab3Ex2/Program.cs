3a66468b6d0149333bd185feec04c7b08baa247d
Robert <robert.kedves.90@gmail.com>
2023-07-08 10:30:49 +0300

    Ex2, Ex3

--- Lab3Ex2/Program.cs
+++ Lab3Ex2/Program.cs
@@ -4,9 +4,38 @@ namespace Lab3Ex2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            /*Scrieti un program care va afisa pozitia unui substring intr un string, ambele siruri de caractere fiind citite de la tastatura*/
+            Console.WriteLine("Introdu o propozitie:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Introdu un caracter sau un text din propozitie:");
+            string substring = Console.ReadLine();
+
+            int pozitia = GasestePozitieSubstring(text, substring);
+
+            if (pozitia != -1)
+            {
+                Console.WriteLine($"Substringul \"{substring}\" se află la pozitia {pozitia} în sirul de caractere.");
+            }
+            else
+            {
+                Console.WriteLine($"Substringul \"{substring}\" nu a fost gasit in sirul de caractere.");
+            }
+        }
+
+        static int GasestePozitieSubstring(string text, string substring)
+        {
+            for (int i = 0; i <= text.Length - substring.Length; i++)
+            {
+                if (text.Substring(i, substring.Length) == substring)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
         }
     }
 }
---
+++ Lab3Ex3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab3Ex3
+{
+    internal class Program
+    {
+        /*Scrieti un program care sa verifice daca un sir de caractere citit de la tastatura este sau nu palindrom*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu un cuvant:");
+            string cuvant = Console.ReadLine();
+
+            if (EstePalindrom(cuvant))
+            {
+                Console.WriteLine($"{cuvant} ESTE palindrom");
+            }
+            else
+            {
+                Console.WriteLine($"{cuvant} NU ESTE palindrom");
+            }
+        }
+
+        static bool EstePalindrom(string cuvant)
+        {
+            int start = 0;
+            int sfarsit = cuvant.Length - 1;
+
+            while (start < sfarsit)
+            {
+                if (cuvant[start] != cuvant[sfarsit])
+                    return false;
+
+                start++;
+                sfarsit--;
+            }
+
+            return true;
+        }
+    }
+}
